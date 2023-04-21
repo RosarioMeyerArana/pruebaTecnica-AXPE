ef146a81708177b77d56ec8c83389392f7d59b4b
rosario meyer arana <rosario.meyera@gmail.com>
2023-04-21 10:48:56 +0200

    handle fetching from app and adding a map function for better performance

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -3,16 +3,15 @@ import Layout from "../components/layout/Layout";
 import classes from "./../components/meetups/MeetupList.module.css";
 
 
-export default function AllMeetupsPage() {
+export default function AllMeetupsPage({data}) {
   return (
     <Layout>
       <section>
         <h1>All Meetups</h1>
         <ul className={classes.list}>
-          <MeetupItem />
-          <MeetupItem />
-          <MeetupItem />
-          <MeetupItem />
+          {
+            data?.map((item) => <MeetupItem key={item.id} item={item} /> )
+          }
         </ul>
       </section>
     </Layout>
