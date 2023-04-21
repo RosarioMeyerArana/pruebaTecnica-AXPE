40a5ed77314519af5049e0b9aa7dab3d0fd2d3be
rosario meyer arana <rosario.meyera@gmail.com>
2023-04-21 11:38:05 +0200

    header animation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,11 +1,31 @@
-// import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
-
+import { useState, useEffect } from 'react';
 import classes from "./MainNavigation.module.css";
 import { NavLink } from 'react-router-dom';
 
-export default function MainNavigation({ setPage }) {
+export default function MainNavigation() {
+  const [showNav, setShowNav] = useState(true)
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY
+
+    if(currentScrollPos > prevScrollPos){
+        setShowNav(false)
+    }else{
+        setShowNav(true)
+    }
+
+    setPrevScrollPos(currentScrollPos)
+}
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll)
+    })
+
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header className={`${classes.header} ${showNav ? classes.navShow : classes.navHide}`} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
